37fce967be5ac976d1f7da48989df2e5c9db8b89
Lokdeep <141442949+lokdeepadimulam@users.noreply.github.com>
2025-09-08 22:20:34 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,22 +1,23 @@
 import cv2
 from ultralytics import YOLO
 
-#extract middle frame
+#This function extracts the frame at the middle of the vid
 def extract_keyframe(video_path):
     cap = cv2.VideoCapture(video_path)
+    #check for any errors
     if not cap.isOpened():
         print("Error: cannot open video")
         return None
 
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) #getting total count of frames to find middle frame
     if total_frames == 0:
         print("Error: video has no frames")
         return None
 
     frame_idx = total_frames // 2  # Middle frame
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx)
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_idx) #setting and reading the middle frae
     ok, frame = cap.read()
-    cap.release()
+    cap.release() #releasing to free resources
 
     if not ok:
         print("Error: cannot read frame")
@@ -31,23 +32,24 @@ def detect_objects(frame, model, labels, conf_thresh=0.35):
     for r in results:
         for box in r.boxes:
             conf = float(box.conf) #checks confidence of every detection and only considers it when above 0.35
-            cls = int(box.cls)
+            cls = int(box.cls) #get class of detected objects
             name = r.names.get(cls, "unknown") #gets the objects that have been detected
             if conf > conf_thresh and name in labels:
-                found.add(name)
+                found.add(name) #returns detections that have passed the threshold
     return found
 
-
+#Main code snippet that calls other functions
 def check_video(video_path):
     print(f"Checking video: {video_path}")
 
     # Load models
-    weapons_model = YOLO("./runs/detect/Normal_Compressed/weights/best.pt")  # yolo model trained on weapons
+    weapons_model = YOLO("./runs/detect/Normal_Compressed/weights/best.pt")  # yolo model trained extensively on weapons dataset
+    # Model: https://github.com/JoaoAssalim/Weapons-and-Knives-Detector-with-YOLOv8
     general_model = YOLO("yolov8n.pt")  # general-purpose YOLOv8 model
 
-    # list prohibited items
+    # list of prohibited items
     prohibited = {
-        "weapons": ["knife", "knives", "gun", "pistol", "rifle", "sword"],
+        "weapons": ["knife", "knives", "gun", "guns", "pistol", "rifle", "sword"],
         "violence": ["fight", "punch", "kick"],
         "explicit": ["nude", "sexual", "porn", "sex"],
         "explosives": ["bomb", "grenade", "explosive", "tnt", "dynamite"]
@@ -80,7 +82,6 @@ def check_video(video_path):
         print("No prohibited items detected.")
         return {"status": "safe", "items": []}
 
-if __name__ == "__main__":
-    video_file = "3.mp4"
-    result = check_video(video_file)
-    print("Result:", result)
+video_file = "3.mp4"
+result = check_video(video_file)
+print("Result:", result)
